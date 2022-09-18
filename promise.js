4f92a2d255386d81e644c5704e8d8b002d89c2c4
jbahisindra <jbahisindra@gmail.com>
2022-09-18 23:51:58 +0700

    progres

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async(promiseOutput) => {
+  return new Promise((resolve, reject) => {
+    if (promiseOutput == 'marah') {
+      const countMarah = (await Promise.all(promiseTheaterIXX, promiseTheaterVGC)).filter(hasil => 'marah')
+      return resolve(countMarah);
+    }
+    else if (promiseOutput == 'tidak marah') {
+      const countTidakMarah = (await Promise.all(promiseTheaterIXX, promiseTheaterVGC)).filter(hasil => 'tidak marah')
+      return reject(countMarah);
+    }
+  });
+}
 
 module.exports = {
   promiseOutput,
